489a52a54132d51ef6e90368bc5300c7a3b5ac63
Sergey-py24 <chuvachoc2009@yandex.ru>
2024-12-17 19:19:24 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -4,11 +4,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-async def welcome() -> dict:
+async def welcome():
     return {'message': 'Главная страница'}
 
 @app.get('/user/admin')
-async def get_admin() -> dict:
+async def get_admin():
     return {'message': 'Вы вошли как администратор'}
 
 @app.get('/user/{user_id}')
@@ -16,5 +16,5 @@ async def get_user(user_id: int):
     return {'message': f'Вы вошли как пользователь № {user_id}'}
 
 @app.get('/user')
-async def info(user_name: str, age: int) -> dict:
+async def info(user_name: str, age: int):
     return {'message': f'Информация о пользователе.Имя: {user_name}, Возраст: {age}'}
