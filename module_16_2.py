d3fd82a2bf632db151264fbb1bd4d30c212bc254
Sergey-py24 <chuvachoc2009@yandex.ru>
2024-12-19 15:40:29 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+
+app = FastAPI()
+
+@app.get('/')
+async def welcome():
+    return 'Главная страница'
+
+@app.get('/user/admin')
+async def welcome():
+    return 'Вы вошли как администратор'
+
+@app.get('/user/{user_id}')
+async def get_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='11')]):
+    return  f'Вы вошли как пользователь № {user_id}'
+
+@app.get('/user/{user_name}/{age}')
+async def info(user_name: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='Bob')]
+               , age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='99')]):
+    return  f'Информация о пользователе.Имя: {user_name}, Возраст: {age}'
+
