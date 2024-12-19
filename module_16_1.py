05bd02bd84c42ca7f374be8aa487c7ae86313df6
Sergey-py24 <chuvachoc2009@yandex.ru>
2024-12-19 10:07:37 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -12,9 +12,9 @@ async def get_admin():
     return {'message': 'Вы вошли как администратор'}
 
 @app.get('/user/{user_id}')
-async def get_user(user_id: int):
+async def get_user(user_id):
     return {'message': f'Вы вошли как пользователь № {user_id}'}
 
 @app.get('/user')
-async def info(user_name: str, age: int):
+async def info(user_name, age):
     return {'message': f'Информация о пользователе.Имя: {user_name}, Возраст: {age}'}
