c7a732fa74afa0e28611f6726317e853186f55e5
Sergey-py24 <chuvachoc2009@yandex.ru>
2024-12-24 21:18:56 +0300

    Add files via upload

--- module_16_4.py
+++ module_16_4.py
@@ -1,9 +1,7 @@
 
 
-
-
 from fastapi import FastAPI, Path, HTTPException
-from typing import Annotated, List
+from typing import Annotated, List, Type
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -24,30 +22,31 @@ def get_users() -> List[User]:
     return users
 
 @app.post("/user/{username}/{age}")
-def post_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
-                    age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
+def post_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
+    example='Bobby')], age: Annotated[int, Path(ge=18, le=120, description='Enter age',
+    example='20')]) -> User:
     user_id = (users[-1].id + 1) if users else 1
     user = User(id=user_id, username=username, age=age)
     users.append(user)
     return user
 
 @app.put("/user/{user_id}/{username}/{age}")
-def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')],
-                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
-                      age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> User:
-    try:
-        for user in users:
-            if user.id == user_id:
-                user.username = username
-                user.age = age
-                return user
-    except IndexError:
-        raise HTTPException(status_code=404, detail='User was not found')
+def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')],
+     username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
+     example='Bobby')],age: Annotated[int, Path(ge=18, le=120, description='Enter age',
+     example='33')]) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail='User was not found')
 
 @app.delete("/user/{user_id}")
-def delete_user(user_id: Annotated[int, Path(ge=1, description="Enter user_id")]):
-    try:
-        users.pop(user_id - 1)
-        return f"User with ID {user_id} has been deleted"
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User was not found")
+def delete_user(user_id: Annotated[int, Path(ge=1, description="Enter user_id",
+                example='2')]):
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
